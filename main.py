4130734f763ce756d5bfc8b25d2636f32844aa20
mastergunk <markusdanp@gmail.com>
2025-05-03 18:45:36 +0200

    Working ship that moves with wasd

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
+#from player import Player2
 
 
 
@@ -14,16 +16,28 @@ def main():
 	print (f"Screen height: {SCREEN_HEIGHT}")
 
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	color = (0, 0, 0)
-
 	frames_per_second = pygame.time.Clock()
 	dt = 0
+	
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
 
+	#Player2.containers = (updatable, drawable)
+	Player.containers = (updatable, drawable)
+	ship = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2)) # Object is used via groups
+	#ship2 = Player2((SCREEN_WIDTH /3), (SCREEN_HEIGHT / 3))
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-		screen.fill(color)
+		
+		screen.fill("black")
+
+		updatable.update(dt)
+		
+		for drawabales in drawable:
+			drawabales.draw(screen)
+
 		pygame.display.flip()
 		frames_per_second.tick(60)
 		dt = (frames_per_second.get_time() / 1000)
